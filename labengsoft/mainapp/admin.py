6847ea18aa1e699789fe051d2ac62c0335ec469a
Lucas Pereira <lucas.pereira6c@gmail.com>
2018-09-09 01:14:37 -0300

    Adicionados os models para administração.

--- labengsoft/mainapp/admin.py
+++ labengsoft/mainapp/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Pessoa
+from .models import *
 # Register your models here.
 
-admin.site.register(Pessoa)
+admin.site.register(SigepEnvironment)
+admin.site.register(Servico)
+admin.site.register(CartaoPostagem)
+admin.site.register(Endereco)
+admin.site.register(GrupoDestinatario)
+admin.site.register(Telefone)
+admin.site.register(Destinatario)
+admin.site.register(Remetente)
+admin.site.register(Embalagem)
+admin.site.register(ObjetoPostal)
+admin.site.register(PreListaPostagem)
