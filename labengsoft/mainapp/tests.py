0af253d725bcda280294c833bca9e0caadef5b18
Fabiano <fabiano.fa04@gmail.com>
2018-09-11 21:21:37 -0300

    Corrige nomenclatura do teste

--- labengsoft/mainapp/tests.py
+++ labengsoft/mainapp/tests.py
@@ -1,17 +1,14 @@
 import unittest
 from django.test import Client
 
-# Create your tests here.
-
-
 class LoginTest(unittest.TestCase):
     def setUp(self):
         # Every test needs a client.
         self.client = Client()
 
-    def simple_login_test(self):
+    def test_details(self):
         # Issue a GET request.
-        response = self.client.post('/admin/login', {'username': 'admin', 'password': '123456'}, follow=True)
+        response = self.client.post('/admin/login', {'username': 'admin', 'password' : '123456'}, follow=True)
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
