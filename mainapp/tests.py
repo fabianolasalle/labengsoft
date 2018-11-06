3e04b08f4b93d7b001e83d7c8b2a5a633cdf817f
Fabiano <fabiano.fa04@gmail.com>
2018-11-06 17:50:01 -0200

    Adiciona testes

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -6,12 +6,55 @@ class LoginTest(unittest.TestCase):
         # Every test needs a client.
         self.client = Client()
 
-    def test_details(self):
+    def test_login(self):
         # Issue a GET request.
-        response = self.client.post('/admin/login', {'username': 'admin', 'password' : '123456'}, follow=True)
+        response = self.client.post('/admin', {'username': 'admin', 'password' : 'admin'}, follow=True)
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
 
+
         # Check that the rendered context contains 5 customers.
         #self.assertEqual(len(response.context['customers']), 5)
+
+    def test_access_list(self):
+        response = self.client.post('/admin', {'username': 'admin', 'password': 'admin'}, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        urls = [
+            '/admin/mainapp/cartaopostagem/',
+            '/admin/mainapp/destinatario/',
+            '/admin/mainapp/embalagem/',
+            '/admin/mainapp/endereco/',
+            '/admin/mainapp/grupodestinatario/',
+            '/admin/mainapp/objetopostal/',
+            '/admin/mainapp/prelistapostagem/',
+            '/admin/mainapp/remetente/',
+            '/admin/mainapp/servico/',
+            '/admin/mainapp/sigepenvironment/',
+            '/admin/mainapp/telefone/'
+        ]
+
+        for url in urls:
+            response = self.client.get(url, follow=True)
+            self.assertEqual(response.status_code, 200)
+
+    def test_access_add(self):
+        response = self.client.post('/admin', {'username': 'admin', 'password': 'admin'}, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        urls = ['/admin/mainapp/cartaopostagem/add/',
+            '/admin/mainapp/destinatario/add/',
+            '/admin/mainapp/embalagem/add/',
+            '/admin/mainapp/grupodestinatario/add/',
+            '/admin/mainapp/objetopostal/add/',
+            '/admin/mainapp/prelistapostagem/add/',
+            '/admin/mainapp/remetente/add/',
+            '/admin/mainapp/servico/add/',
+            '/admin/mainapp/sigepenvironment/add/',
+            '/admin/mainapp/telefone/add/'
+        ]
+
+        for url in urls:
+            response = self.client.get(url, follow=True)
+            self.assertEqual(response.status_code, 200)
